3596eeb161935a73398888c0673520c25f04f711
Arefjev Grigory <grinya.guitar@gmail.com>
2014-01-10 00:48:23 +0400

    get active ap fix

--- dbus_nm_active_ap.pl
+++ dbus_nm_active_ap.pl
@@ -3,14 +3,22 @@ use strict;
 use warnings;
 use Net::DBus;
 
+use Data::Dumper;
+
 # locally
 $ENV{DISPLAY} = ':0.0';
 
-my $rWlan = Net::DBus->session()
-    ->get_service('org.freedesktop.NetworkManager.Device.Wireless')
-    ->get_object('/org/freedesktop/NetworkManager/Device/Wireless', 'org.freedesktop.NetworkManager.Device.Wireless');
-    
-warn $rWlan->ActiveAccessPoint()->Ssid();
+my $rNM = Net::DBus->system()
+    ->get_service('org.freedesktop.NetworkManager');
+my $sWlan0 = $rNM
+    ->get_object('/org/freedesktop/NetworkManager', 'org.freedesktop.NetworkManager')
+    ->GetDeviceByIpIface('wlan0');
+my $rWlan0 = $rNM
+    ->get_object($sWlan0, 'org.freedesktop.NetworkManager.Device.Wireless');
+my $rAP = $rNM
+    ->get_object($rWlan0->ActiveAccessPoint(), 'org.freedesktop.NetworkManager.AccessPoint');
+
+print join('', map {chr($_)} @{ $rAP->Ssid() });
 
 exit 0;
 
